283318692943003489861f439113d32ced9c63bf
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-06-22 17:07:01 +0100

    add headline, company, and image_url fields to UserFactory; create PostFactory for post model

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -30,6 +30,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'headline'=> fake()->jobTitle(),
+            'company'=>fake()->company(),
+            'image_url'=>fake()->imageUrl(100,100,'people'),
         ];
     }
 
