c7f61bcde45bf02ace0206803be5812b3daaf232
AITABBOUyoussef <aitabbouyoussef111@gmail.com>
2026-06-22 16:26:06 +0100

    refactor Post model to include HasFactory trait and define user relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    
-}
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'content',
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    }
